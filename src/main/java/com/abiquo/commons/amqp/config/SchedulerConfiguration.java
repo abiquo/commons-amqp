f69e19b03eae035685e7cd5c8386d163a4697dd2
enricruiz <morelies@gmail.com>
2013-02-13 10:39:29 +0100

    AMQPConsumer and AMQPProducer are concrete classes that can produce or consume any Serializable object given a concrete AMQPConfiguration

---
+++ src/main/java/com/abiquo/commons/amqp/config/SchedulerConfiguration.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2008 - Abiquo Holdings S.L. All rights reserved.
+ *
+ * Please see /opt/abiquo/tomcat/webapps/legal/ on Abiquo server
+ * or contact contact@abiquo.com for licensing information.
+ */
+package com.abiquo.commons.amqp.config;
+
+import java.io.IOException;
+
+import com.abiquo.commons.amqp.AMQPConfiguration;
+import com.rabbitmq.client.Channel;
+
+public class SchedulerConfiguration extends AMQPConfiguration
+{
+    private static final String SCHEDULER_EXCHANGE = "abiquo.scheduler";
+
+    private static final String SCHEDULER_REQUESTS_QUEUE = "abiquo.scheduler.requests";
+
+    private static final String SCHEDULER_FAST_QUEUE = "abiquo.scheduler.fast.requests";
+
+    private static final String SCHEDULER_SLOW_QUEUE = "abiquo.scheduler.slow.requests";
+
+    @Override
+    public void declareExchanges(final Channel channel) throws IOException
+    {
+        channel.exchangeDeclare(getExchange(), TopicExchange, Durable);
+    }
+
+    @Override
+    public void declareQueues(final Channel channel) throws IOException
+    {
+        // Declare the fast router queue for FREE and UPDATE requests
+        channel.queueDeclare(SCHEDULER_FAST_QUEUE, Durable, NonExclusive, NonAutodelete, null);
+        channel.queueBind(SCHEDULER_FAST_QUEUE, SCHEDULER_EXCHANGE, SCHEDULER_FAST_QUEUE);
+
+        // Declare the slow router queue for SCHEDULE requests
+        channel.queueDeclare(SCHEDULER_SLOW_QUEUE, Durable, NonExclusive, NonAutodelete, null);
+        channel.queueBind(SCHEDULER_SLOW_QUEUE, SCHEDULER_EXCHANGE, SCHEDULER_SLOW_QUEUE);
+
+        // Declare the main scheduler queue
+        channel.queueDeclare(SCHEDULER_REQUESTS_QUEUE, Durable, NonExclusive, NonAutodelete, null);
+        channel.queueBind(SCHEDULER_REQUESTS_QUEUE, SCHEDULER_EXCHANGE, SCHEDULER_REQUESTS_QUEUE);
+    }
+
+    @Override
+    public String getExchange()
+    {
+        return SCHEDULER_EXCHANGE;
+    }
+
+    @Override
+    public String getRoutingKey()
+    {
+        return SCHEDULER_REQUESTS_QUEUE;
+    }
+
+    @Override
+    public String getQueue()
+    {
+        return SCHEDULER_REQUESTS_QUEUE;
+    }
+
+    public String getFastRoutingKey()
+    {
+        return SCHEDULER_FAST_QUEUE;
+    }
+
+    public String getFastQueue()
+    {
+        return SCHEDULER_FAST_QUEUE;
+    }
+
+    public String getSlowRoutingKey()
+    {
+        return SCHEDULER_FAST_QUEUE;
+    }
+
+    public String getSlowQueue()
+    {
+        return SCHEDULER_FAST_QUEUE;
+    }
+}
