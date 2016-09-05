2e76ebbb3e700a7e6244358abe5f8fa7103fee43
Enric Ruiz <enric.ruiz@abiquo.com>
2016-09-05 17:05:43 +0200

    VirtualMachineActionPlanExecutionRequest AMQP configuration

---
+++ src/main/java/com/abiquo/commons/amqp/config/VirtualMachineActionPlanExecutionRequestConfiguration.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2008 - Abiquo Holdings S.L. All rights reserved.
+ *
+ * Please see /opt/abiquo/tomcat/webapps/legal/ on Abiquo server
+ * or contact contact@abiquo.com for licensing information.
+ */
+package com.abiquo.commons.amqp.config;
+
+import java.io.IOException;
+
+import com.abiquo.commons.amqp.AMQPConfiguration;
+import com.rabbitmq.client.Channel;
+
+public class VirtualMachineActionPlanExecutionRequestConfiguration extends AMQPConfiguration
+{
+    private static final String EXCHANGE = "abiquo.vm.actionplan";
+
+    private static final String ROUTING_KEY = "abiquo.vm.actionplans";
+
+    private static final String QUEUE = ROUTING_KEY;
+
+    @Override
+    public void declareExchanges(final Channel channel) throws IOException
+    {
+        channel.exchangeDeclare(getExchange(), DirectExchange, Durable);
+    }
+
+    @Override
+    public void declareQueues(final Channel channel) throws IOException
+    {
+        channel.queueDeclare(getQueue(), Durable, !Exclusive, !Autodelete, null);
+        channel.queueBind(getQueue(), getExchange(), getRoutingKey());
+    }
+
+    @Override
+    public String getExchange()
+    {
+        return EXCHANGE;
+    }
+
+    @Override
+    public String getRoutingKey()
+    {
+        return ROUTING_KEY;
+    }
+
+    @Override
+    public String getQueue()
+    {
+        return QUEUE;
+    }
+}
