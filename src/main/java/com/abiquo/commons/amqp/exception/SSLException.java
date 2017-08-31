6f664168cbdddfe176ffe8418cd6f0cab47d94d1
Enric Ruiz <enric.ruiz@abiquo.com>
2017-08-31 17:29:59 +0200

    Added missing license

--- src/main/java/com/abiquo/commons/amqp/exception/SSLException.java
+++ src/main/java/com/abiquo/commons/amqp/exception/SSLException.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2008 - Abiquo Holdings S.L. All rights reserved.
+ *
+ * Please see /opt/abiquo/tomcat/webapps/legal/ on Abiquo server
+ * or contact contact@abiquo.com for licensing information.
+ */
 package com.abiquo.commons.amqp.exception;
 
 public class SSLException extends Exception
