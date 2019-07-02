fc271e8e5e520ddcf57d9e714e145428d24ff890
Albert Puig <apuig@abiquo.com>
2019-07-02 15:44:44 +0200

    tarantino now uses commons-amqp ABICLOUDPREMIUM-11852

--- src/main/java/com/abiquo/commons/amqp/config/TarantinoRequestConfiguration.java
+++ src/main/java/com/abiquo/commons/amqp/config/TarantinoRequestConfiguration.java
@@ -6,6 +6,9 @@
  */
 package com.abiquo.commons.amqp.config;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.System.getProperty;
+
 public class TarantinoRequestConfiguration extends DatacenterRequestConfiguration
 {
     private static final String TARANTINO_ROUTING_KEY = "virtualfactory";
@@ -13,5 +16,20 @@ public class TarantinoRequestConfiguration extends DatacenterRequestConfiguratio
     public TarantinoRequestConfiguration(final String datacenterId)
     {
         super(datacenterId, TARANTINO_ROUTING_KEY);
+    }
+
+    /**
+     * qos "quality of service", or prefetch count. The number of non-acknowledged messages a
+     * channel can receive. If set to one then the consumer using this channel will not receive
+     * another message until it has acknowledged or rejected its current message. This feature is
+     * commonly used as a load-balancing strategy using multiple consumers and a shared queue.
+     */
+    private static final Integer DATACENTER_REQUEST_CONSUMER_PREFETCH =
+        parseInt(getProperty("abiquo.virtualfactory.prefetch", "50"));
+
+    @Override
+    public int getPrefetchCount()
+    {
+        return DATACENTER_REQUEST_CONSUMER_PREFETCH;
     }
 }
