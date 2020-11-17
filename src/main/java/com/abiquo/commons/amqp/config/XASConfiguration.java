662a490b71622d385a201e031e5a8748c32bae39
Albert Puig <apuig@abiquo.com>
2020-11-17 10:37:50 +0100

    xas amqp config

---
+++ src/main/java/com/abiquo/commons/amqp/config/XASConfiguration.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2008 - Abiquo Holdings S.L. All rights reserved.
+ *
+ * Please see /opt/abiquo/tomcat/webapps/legal/ on Abiquo server
+ * or contact contact@abiquo.com for licensing information.
+ */
+package com.abiquo.commons.amqp.config;
+
+import com.abiquo.commons.amqp.AMQPConfiguration;
+import com.abiquo.commons.amqp.AMQPFlags;
+
+public class XASConfiguration
+{
+
+    public static class RequestConfiguration extends AMQPConfiguration
+    {
+        private static final String EXCHANGE = "abiquo.xas";
+
+        private String datacenterId;
+
+        public RequestConfiguration(final String datacenterId)
+        {
+            this.datacenterId = datacenterId;
+        }
+
+        @Override
+        public AMQPFlags getFlags()
+        {
+            return AMQPFlags.direct() //
+                .exchangeDurable(true) //
+                .queueDurable(true) //
+                .queueExclusive(false) //
+                .queueAutoDelete(false) //
+                .build();
+        }
+
+        @Override
+        public String getExchange()
+        {
+            return EXCHANGE;
+        }
+
+        @Override
+        public String getRoutingKey()
+        {
+            return EXCHANGE.concat(".requests.").concat(datacenterId);
+        }
+
+        @Override
+        public String getQueue()
+        {
+            return getRoutingKey();
+        }
+    }
+
+    // FIXME xas - responses goes to the same queue as nars
+    public static class ResponseConfiguration extends NARSConfiguration.ResponseConfiguration
+    {
+
+    }
+}
